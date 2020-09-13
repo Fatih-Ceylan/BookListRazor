8acfe4c1fdcfe5d9552b5116283745ca12a103a0
Fatih-Ceylan <fceylan001@gmail.com>
2020-09-13 15:11:47 +0300

    Update ApplicationDbContextModelSnapshot.cs

--- BookListRazor/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BookListRazor/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using BookListRazor.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookListRazor.Migrations
 {
@@ -14,31 +13,31 @@ namespace BookListRazor.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.6")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion" , "3.1.6")
+                .HasAnnotation("Relational:MaxIdentifierLength" , 128)
+                .HasAnnotation("SqlServer:ValueGenerationStrategy" , SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("BookListRazor.Model.Book", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+            modelBuilder.Entity("BookListRazor.Model.Book" , b =>
+                 {
+                     b.Property<int>("Id")
+                         .ValueGeneratedOnAdd()
+                         .HasColumnType("int")
+                         .HasAnnotation("SqlServer:ValueGenerationStrategy" , SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Author")
-                        .HasColumnType("nvarchar(max)");
+                     b.Property<string>("Author")
+                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("ISBN")
-                        .HasColumnType("nvarchar(max)");
+                     b.Property<string>("ISBN")
+                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                     b.Property<string>("Name")
+                         .IsRequired()
+                         .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Id");
+                     b.HasKey("Id");
 
-                    b.ToTable("Book");
-                });
+                     b.ToTable("Book");
+                 });
 #pragma warning restore 612, 618
         }
     }
