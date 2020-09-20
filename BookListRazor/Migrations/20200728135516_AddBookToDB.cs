b55548ed88eb608b3b0ac184248b13a3108d2347
Fatih-Ceylan <fceylan001@gmail.com>
2020-09-20 23:25:58 +0300

    Update 20200728135516_AddBookToDB.cs

--- BookListRazor/Migrations/20200728135516_AddBookToDB.cs
+++ BookListRazor/Migrations/20200728135516_AddBookToDB.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
-
 namespace BookListRazor.Migrations
 {
     public partial class AddBookToDB : Migration
@@ -7,20 +6,19 @@ namespace BookListRazor.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Book",
+                name: "Book" ,
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(nullable: false),
+                        .Annotation("SqlServer:Identity" , "1, 1") ,
+                    Name = table.Column<string>(nullable: false) ,
                     Author = table.Column<string>(nullable: true)
-                },
+                } ,
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.PrimaryKey("PK_Book" , x => x.Id);
                 });
         }
-
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
